cbf048afd2d813b3d87bcfd849ec6560ecc3e97a
Vesisss <g.santaniello25@studenti.unisa.it>
2025-05-20 15:16:31 +0200

    Update admin.h

--- admin.h
+++ admin.h
@@ -1,8 +1,17 @@
 #ifndef ADMIN_H
 #define ADMIN_H
 
+#include "hash.h"
 #include "list.h"
 
-void adminMenu(List *vehicles);
+/*
+ * Displays the admin interface menu and handles admin operations,
+ * including managing vehicles, users, and bookings.
+ *
+ * vehicleTable: hash table containing all vehicles
+ * userTable: hash table containing all users
+ * bookingList: list of all bookings
+ */
+void adminMenu(HashTable* vehicleTable, HashTable* userTable, List bookingList);
 
 #endif
