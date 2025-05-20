b6f4a2cf891a68817bf3c313f94b68c5cb126c45
Vesisss <g.santaniello25@studenti.unisa.it>
2025-05-20 15:45:26 +0200

    Update list.h

--- list.h
+++ list.h
@@ -1,15 +1,50 @@
 #ifndef LIST_H
 #define LIST_H
 
-#include <stdio.h>
+// Opaque pointer to the list structure
+typedef struct ListStruct* List;
 
-typedef struct List *List;
+/*
+ * Creates and initializes an empty list.
+ *
+ * returns: pointer to the new List
+ */
+List createList();
 
-List CreateList();
-void DeleteValue(List List, int Position);
-int InsertValue(List List, void *Item);
-void *GetItem(List List, int Position);
-int FindValue(List List, void *Value);
-int GetSize(List List);
+/*
+ * Inserts a new item at the beginning of the list.
+ *
+ * list: pointer to the list
+ * data: generic pointer to the item to insert
+ *
+ * returns: 0 on success, -1 on allocation failure
+ */
+int insertList(List list, void* data);
+
+/*
+ * Retrieves an item from the list at the specified index.
+ *
+ * list: pointer to the list
+ * index: index of the item (0-based)
+ *
+ * returns: pointer to the item, or NULL if index is invalid
+ */
+void* getItem(List list, int index);
+
+/*
+ * Returns the number of elements in the list.
+ *
+ * list: pointer to the list
+ *
+ * returns: size of the list, or 0 if list is NULL
+ */
+int getSize(List list);
+
+/*
+ * Frees all memory used by the list and its nodes.
+ *
+ * list: pointer to the list
+ */
+void freeList(List list);
 
 #endif
