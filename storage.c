12e9675dc85df78c431609154258247b6553f9b8
Vesisss <g.santaniello25@studenti.unisa.it>
2025-05-27 16:20:17 +0200

    Update storage.c

--- storage.c
+++ storage.c
@@ -5,45 +5,51 @@
 #include "vehicle.h"
 #include "user.h"
 #include "booking.h"
+#include "hash.h"
+#include "list.h"
 
 /*
  * File: storage.c
  * ---------------
- * Handles persistent storage for vehicles, users, and bookings.
- * Provides functions to load and save data to and from text files.
+ * Manages file I/O for vehicles, users, and bookings.
+ * Provides functions to load and save data from/to plain text files.
  */
 
 /*
- * Function: LoadVehiclesFromFile
- * ------------------------------
- * Loads vehicle data from a text file and inserts each record into the hash table.
+ * Loads vehicle data from a file and inserts each entry into the hash table.
  *
  * table: hash table where vehicles are stored
- * filename: name of the file to read from
+ * filename: path to the vehicle file
  */
-void LoadVehiclesFromFile(HashTable* table, const char* filename) {
+ void LoadVehiclesFromFile(HashTable* table, const char* filename) {
     FILE* file = fopen(filename, "r");
     if (!file) return;
 
-    char plate[20], type[20], fuel[20], brand[30], model[30], location[30];
-    float cost;
-    int year, seats;
+    char line[256];
+    while (fgets(line, sizeof(line), file)) {
+        char plate[50], type[50], fuel[50], brand[50], model[50], location[50];
+        float cost;
+        int year, seats;
 
-    while (fscanf(file, "%s %f %s %d %s %s %s %d %s",
-                  plate, &cost, type, &seats, fuel, brand, model, &year, location) == 9) {
-        Vehicle* v = createVehicle(plate, cost, seats, type, fuel, brand, model, year, location);
-        insertVehicle(table, v);
+        if (sscanf(line, "%s %f %s %d %s %s %s %d %s",
+                   plate, &cost, type, &seats,
+                   fuel, brand, model, &year, location) == 9) {
+            Vehicle* v = createVehicle(plate, cost, seats, type, fuel, brand, model, year, location);
+            insertVehicle(table, v);
+        }
     }
+
     fclose(file);
 }
 
+
+
+
 /*
- * Function: LoadUsersFromFile
- * ---------------------------
- * Loads user data from a text file and inserts each user into the hash table.
+ * Loads user data from a file and inserts each entry into the hash table.
  *
  * table: hash table where users are stored
- * filename: name of the file to read from
+ * filename: path to the user file
  */
 void LoadUsersFromFile(HashTable* table, const char* filename) {
     FILE* file = fopen(filename, "r");
@@ -54,19 +60,18 @@ void LoadUsersFromFile(HashTable* table, const char* filename) {
         User* u = createUser(username, firstName, lastName);
         insertUser(table, u);
     }
+
     fclose(file);
 }
 
 /*
- * Function: LoadBookingsFromFile
- * ------------------------------
- * Loads booking records from a file and inserts them into the booking list.
- * Each booking is reconstructed using the vehicle reference.
+ * Loads booking data from a file and inserts each booking into the list.
+ * Uses vehicle references to reconstruct each booking correctly.
  *
  * list: list where bookings are stored
- * filename: name of the file to read from
- * vehicleTable: table used to retrieve vehicles by plate
- * userTable: unused, may be removed
+ * filename: path to the booking file
+ * vehicleTable: hash table used to retrieve vehicles by plate
+ * userTable: hash table used to validate users (optional)
  */
 void LoadBookingsFromFile(List list, const char* filename, HashTable* vehicleTable, HashTable* userTable) {
     FILE* file = fopen(filename, "r");
@@ -85,64 +90,67 @@ void LoadBookingsFromFile(List list, const char* filename, HashTable* vehicleTab
             insertList(list, b);
         }
     }
+
     fclose(file);
 }
 
+
 /*
- * Function: SaveVehiclesToFile
- * ----------------------------
- * Saves all vehicles from the hash table to a text file.
+ * Callback function for saving vehicles to file.
+ */
+ void vehicleSaver(const char* key, void* value, void* filePtr) {
+    Vehicle* v = (Vehicle*)value;
+    FILE* file = (FILE*)filePtr;
+
+    fprintf(file, "%s %.2f %s %d %s %s %s %d %s\n",
+            getVehiclePlate(v), getVehicleCost(v), getVehicleType(v),
+            getVehicleSeats(v), getVehicleFuel(v), getVehicleBrand(v),
+            getVehicleModel(v), getVehicleYear(v), getVehicleLocation(v));
+}
+
+
+/*
+ * Saves all vehicles stored in the hash table to a text file.
  *
- * table: hash table containing vehicles
- * filename: name of the file to write to
+ * table: hash table containing the vehicles
+ * filename: path to the output file
  */
 void SaveVehiclesToFile(HashTable* table, const char* filename) {
     FILE* file = fopen(filename, "w");
     if (!file) return;
-
-    for (int i = 0; i < TABLE_SIZE; i++) {
-        HashNode* node = table->table[i];
-        while (node) {
-            Vehicle* v = (Vehicle*)node->value;
-            fprintf(file, "%s %.2f %s %d %s %s %s %d %s\n",
-                    v->plate, v->cost, v->type, v->seats,
-                    v->fuel, v->brand, v->model, v->year, v->location);
-            node = node->next;
-        }
-    }
+    forEachHash(table, vehicleSaver, file);
     fclose(file);
 }
 
+
+
 /*
- * Function: SaveUsersToFile
- * -------------------------
- * Saves all users from the hash table to a text file.
+ * Callback function for saving users to file.
+ */
+ void userSaver(const char* key, void* value, void* filePtr) {
+    User* u = (User*)value;
+    FILE* file = (FILE*)filePtr;
+    fprintf(file, "%s %s %s\n", getUserUsername(u), getUserFirstName(u), getUserLastName(u));
+}
+
+/*
+ * Saves all users stored in the hash table to a text file.
  *
- * table: hash table containing users
- * filename: name of the file to write to
+ * table: hash table containing the users
+ * filename: path to the output file
  */
 void SaveUsersToFile(HashTable* table, const char* filename) {
     FILE* file = fopen(filename, "w");
     if (!file) return;
-
-    for (int i = 0; i < TABLE_SIZE; i++) {
-        HashNode* node = table->table[i];
-        while (node) {
-            User* u = (User*)node->value;
-            fprintf(file, "%s %s %s\n", u->username, u->firstName, u->lastName);
-            node = node->next;
-        }
-    }
+    forEachHash(table, userSaver, file);
     fclose(file);
 }
 
 /*
- * Function: SaveBookingsToFile
- * ----------------------------
- * Saves all bookings from the list to a text file.
+ * Saves all bookings stored in the list to a text file.
  *
- * list: list of bookings
- * filename: name of the file to write to
+ * list: list of Booking pointers
+ * filename: path to the output file
  */
 void SaveBookingsToFile(List list, const char* filename) {
     FILE* file = fopen(filename, "w");
@@ -151,8 +159,9 @@ void SaveBookingsToFile(List list, const char* filename) {
     for (int i = 0; i < getSize(list); i++) {
         Booking* b = (Booking*)getItem(list, i);
         fprintf(file, "%s %s %ld %ld %.2f %d\n",
-                b->username, b->plate, b->startTimestamp, b->endTimestamp,
-                b->totalCost, b->isDiscounted);
+                getBookingUsername(b), getBookingPlate(b),
+                getBookingStartTimestamp(b), getBookingEndTimestamp(b),
+                getBookingTotalCost(b), getBookingIsDiscounted(b));
     }
     fclose(file);
 }
