43efa24d0c7bc78813224f6fa7565c4d651688df
Vesisss <g.santaniello25@studenti.unisa.it>
2025-05-30 13:03:55 +0200

    Update admin.c
    upgrading graphical interface

--- admin.c
+++ admin.c
@@ -27,51 +27,51 @@
  void adminMenu(HashTable* vehicleTable, HashTable* userTable, List bookingList) {
     int choice;
     do {
-        printf("\n=== ADMIN MENU ===\n\n");
-        printf("1. Add a new vehicle\n");
-        printf("2. Remove a vehicle\n");
-        printf("3. View all vehicles\n");
-        printf("4. View all users\n");
-        printf("5. View all bookings\n");
-        printf("0. Exit admin mode\n\n");
-        printf("Enter your choice: ");
+        printf("\n\t=== ADMIN MENU ===\n\n");
+        printf("\t1. Add a new vehicle\n");
+        printf("\t2. Remove a vehicle\n");
+        printf("\t3. View all vehicles\n");
+        printf("\t4. View all users\n");
+        printf("\t5. View all bookings\n");
+        printf("\t0. Exit admin mode\n\n");
+        printf("\tEnter your choice: ");
         scanf("%d", &choice); getchar();
 
         switch (choice) {
             case 1:
-                printf("\n--- ADD VEHICLE ---\n\n");
+                printf("\n\t--- ADD VEHICLE ---\n\n");
                 addVehiclePrompt(vehicleTable);
                 printf("\n");
                 break;
 
             case 2:
-                printf("\n--- REMOVE VEHICLE ---\n\n");
+                printf("\n\t--- REMOVE VEHICLE ---\n\n");
                 removeVehiclePrompt(vehicleTable);
                 printf("\n");
                 break;
 
             case 3:
-                printf("\n--- ALL VEHICLES ---\n\n");
+                printf("\n\t--- ALL VEHICLES ---\n\n");
                 if (vehicleTable) {
                     printAllVehicles(vehicleTable);
                 } else {
-                    printf("No vehicles found.\n");
+                    printf("\tNo vehicles found.\n");
                 }
                 printf("\n");
                 break;
 
             case 4:
-                printf("\n--- ALL USERS ---\n\n");
+                printf("\n\t--- ALL USERS ---\n\n");
                 if (userTable) {
                     printAllUsers(userTable);
                 } else {
-                    printf("No users found.\n");
+                    printf("\tNo users found.\n");
                 }
                 printf("\n");
                 break;
 
             case 5:
-                printf("\n--- ALL BOOKINGS ---\n\n");
+                printf("\n\t--- ALL BOOKINGS ---\n\n");
                 if (getSize(bookingList) > 0) {
                     printAllBookings(bookingList);
                 } else {
@@ -81,11 +81,11 @@
                 break;
 
             case 0:
-                printf("\nExiting admin mode...\n\n");
+                printf("\n\tExiting admin mode...\n\n");
                 break;
 
             default:
-                printf("\nInvalid choice. Please try again.\n\n");
+                printf("\n\tInvalid choice. Please try again.\n\n");
         }
 
     } while (choice != 0);
